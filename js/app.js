cae406afbc6e13228ced5656d92fad072dc88e91
MistaTG <nazanator@hotmail.com>
2016-10-27 17:43:27 -0600

    Code and Render Basic State of Character and Enemy

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x ,y ) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -24,12 +26,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x ,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = "images/char-boy.png";
+};
 
+Player.prototype.update = function(dt) {
+
+};
+
+//Player.prototype.handleInput = function() {};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [ newEnemy = new Enemy(1, 225)];
+var player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
